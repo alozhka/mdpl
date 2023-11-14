54953f1b2a64b773f92b2fa1593132edb24ddaa9
Sergey <100339481+alozhka@users.noreply.github.com>
2023-11-14 23:11:31 +0300

    Дописал 3 режима гирлянды

--- lw5/Garland/c/Garland.c
+++ lw5/Garland/c/Garland.c
@@ -4,29 +4,32 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+
 /**
 Режимы работы:
-1. Накопление
-2.
-3.
-4.
-5.
-6.
-7.
+0. Накопление слева направо
+1. Накопление вцентр
+2. Накопление рандомное
 */
-volatile int state = 0;
+volatile int state = 2;
 volatile int i = 0;
 volatile int off_lights_counter = 18;
 
 
-ISR(INT1_vect)
+void reset_garland()
 {
-	state++;
-	if(state == 6) state = 0;
+	PORTB = 0;
+	PORTC = 0;
+	PORTD = (1 << PIND2);
+}
+void fill_garland()
+{
+	PORTB = 0xFF;
+	PORTC = 0xFF;
+	PORTD = 0b11111100;
 }
 
-
-void draw_accumulation()
+void draw_accumulation_to_right()
 {
 	if ((0 <= i) & (i <= 7))
 	{
@@ -42,65 +45,159 @@ void draw_accumulation()
 	}
 	if (i == 14) PORTC &= ~(1 << PINC5);
 	
-	if((14 <= i) && (i <= 16))
+	if((14 <= i) && (i <= 17))
 	{
-		PORTD &= ~(1 << (i - 14 - 1));	// очищаем предыдущий
-		PORTD |= (1 << (i - 14));
+		PORTD &= ~(1 << (i - 10 - 1));	// с пина 4 по пин 7
+		PORTD |= (1 << (i - 10));
 	}
-	if (i == 17)
-	{
-		PORTD &= ~(1 << PIND2);
-		PORTD |= (1 << PIND4);
-	}
-	
 	
 	i++;
-	
 	if(off_lights_counter == 0)
 	{
 		i = 0;
-		PORTB = 0;
-		PORTC = 0;
-		PORTD = (1 << PIND3);
 		off_lights_counter = 18;
+		reset_garland();
 	}
 	if(i == off_lights_counter)
 	{
 		i = 0;
 		off_lights_counter--;
 	}
-	_delay_ms(100);
+	_delay_ms(250);
 }
 
+void draw_accumulation_to_center()
+{
+	//
+	// правая сторона
+	//
+	if ((0 <= i) & (i <= 3))
+	{
+		PORTD &= ~(0x80 >> (i - 1)); // 0b10000000
+		PORTD |= (0x80 >> i);
+	}
+	if (i == 4) PORTD &= ~(1 << PIND4);
+	
+	if((4 <= i) & (i <= 8))
+	{
+		PORTC &= ~(0x20 >> (i - 4 - 1)); // 0b00100000
+		PORTC |= (0x20 >> (i - 4));
+	}
+	
+	//
+	// левая сторона
+	//
+	if((0 <= i) & (i <= 7))
+	{
+		PORTB &= ~(1 << (i - 1)); // 0b00100000
+		PORTB |= (1 << i);
+	}
+	if (i == 8)
+	{
+		PORTB &= ~(1 << PINB7);
+		PORTC |= (1 << PINC0);
+	}
+	
+	
+	i++;
+	if (off_lights_counter == 0)
+	{
+		i = 0;
+		off_lights_counter = 18;
+		reset_garland();
+	}
+	if (i == off_lights_counter / 2)
+	{
+		i = 0;
+		off_lights_counter -= 2;
+	}
+	_delay_ms(250);
+}
+
+void draw_decreasing_to_left()
+{
+	if ((0 <= i) & (i <= 3))
+	{
+		PORTD |= (0x80 >> (i - 1));
+		PORTD &= ~(0x80 >> i); // 0b10000000
+	}
+	if(i == 4) PORTD |= (1 << PIND4);
+	if ((4 <= i) & (i <= 9))
+	{
+		PORTC |= (0x20 >> (i - 4 - 1));
+		PORTC &= ~(0x20 >> (i - 4)); // 0b0010000
+	}
+	if (i == 10) PORTC |= (1 << PINC0);
+	if ((10 <= i) & (i <= 17))
+	{
+		PORTB |= (0x80 >> (i - 10 - 1));
+		PORTB &= ~(0x80 >> (i - 10)); // 0b10000000
+	}
+	
+	i++;
+	if(off_lights_counter == 0)
+	{
+		i = 0;
+		off_lights_counter = 18;
+		fill_garland();
+	}
+	if(i == off_lights_counter)
+	{
+		i = 0;
+		off_lights_counter--;
+	}
+	_delay_ms(250);
+	
+}
+
+ISR(INT0_vect)
+{
+	i = 0;
+	off_lights_counter = 18;
+	PORTB = 0;
+	PORTC = 0;
+	PORTD = (1 << PIND2);
+	
+	
+	state++;
+	if(state == 3) 
+		state = 0;
+	if (state == 2)
+		fill_garland();
+}
 
 void setup(void)
 {
 	DDRB = 0xFF;		// все 8
 	DDRC = 0b01111111;	// + 6
-	DDRD = 0b00011111;	// PIND2 на прерывание по изменению состояения, оставшиеся 4 на гирлянду
+	DDRD = 0b11111100;	// PIND3 на прерывание по изменению состояения, оставшиеся 4 на гирлянду
 	
-	PORTD |= (1 << PIND3);
+	PORTD |= (1 << PIND2);
 	
-	EIMSK |= (1 << INT1);
-	EICRA |= (1 << ISC11);
+	EIMSK |= (1 << INT0);
+	EICRA |= (1 << ISC01);
 	sei();
 }
-
 
 int main(void)
 {
 	setup();
+	PORTB = 0xff;
+	PORTC = 0xff;
+	PORTD = 0xff;
 	
 	while (1)
 	{
 		switch (state)
 		{
 			case 0:
-			draw_accumulation();
-			break;
+				draw_accumulation_to_right();
+				break;
 			case 1:
-			PORTB = 0xFF;
-			break;
+				draw_accumulation_to_center();
+				break;
+			case 2:
+				draw_decreasing_to_left();
 		}
 	}
 }
