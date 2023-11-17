2e45f8a8d87047341b593a40649261101792273e
Sergey <100339481+alozhka@users.noreply.github.com>
2023-11-17 18:12:27 +0300

    Нормально расписал SBCI

--- lw4/MathAndLogic/op6.SBCI/SBCI.asm
+++ lw4/MathAndLogic/op6.SBCI/SBCI.asm
@@ -1,14 +1,42 @@
-reset:
-	rjmp main
-main:
-	nop
 loop:
-	; ввод 
+	nop
+	; ввожу второй аргумент нативно, 
+	; так как команда по своей сути создана, 
+	; для того чтобы сразу вычитать значение,
+	; а не записывать его предварительно в регистр
+
+	; ввод 16
 	in r18, OCR0A
-	in r19, OCR0B
-	; выполнение операции
-	sbci r18,r19
-	; вывод 
+	sbci r18,1
 	out OCR0A, r18
-	out OCR0B, r19
+	
+	; ввод 128
+	in r18, OCR0A
+	sbci r18,0
+	out OCR0A, r18
+	
+	; ввод 128
+	in r18, OCR0A
+	sbci r18,1
+	out OCR0A, r18
+	
+	; ввод 0
+	in r18, OCR0A
+	sbci r18,0
+	out OCR0A, r18
+	
+	; ввод 256
+	in r18, OCR0A
+	sbci r18,1
+	out OCR0A, r18
+	
+	; ввод 0
+	in r18, OCR0A
+	sbci r18,0
+	out OCR0A, r18
+
+	; ввод 127
+	in r18, OCR0A
+	sbci r18,-1
+	out OCR0A, r18
 rjmp loop
