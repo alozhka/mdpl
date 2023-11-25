d3a34d9d1c0b0994eafbc2eed63042c5885d7ef3
Sergey <100339481+alozhka@users.noreply.github.com>
2023-11-25 12:48:45 +0300

    Сделал динамическую индикацию

---
+++ lw6/DinamicIndication/main.c
@@ -0,0 +1,115 @@
+#define F_CPU 1000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+uint8_t segments[] =
+{
+	0b00111111, // 0 - A, B, C, D, E, F
+	0b00000110, // 1 - B, C
+	0b01011011, // 2 - A, B, D, E, G
+	0b01001111, // 3 - A, B, C, D, G
+	0b01100110, // 4 - B, C, F, G
+	0b01101101, // 5 - A, C, D, F, G
+	0b01111101, // 6 - A, C, D, E, F, G
+	0b00000111, // 7 - A, B, C
+	0b01111111, // 8 - A, B, C, D, E, F, G
+	0b01101111, // 9 - A, B, C, D, F, G
+};
+
+void InitPorts(void);
+void send_data(uint8_t data, uint8_t ind);
+void InitTimer0(void);
+void Bin2Dec(uint16_t data);
+
+volatile uint16_t cnt = 0;
+volatile uint8_t switch_state = 0;
+volatile uint8_t bcd_buffer[] = { 0, 0, 0, 0 };
+	
+
+int main(void)
+{
+	InitPorts();
+	InitTimer0();
+	EIMSK |= (1 << INT0); //ВключитьINT0
+	EICRA |= (1 << ISC01); //Настройка INT0 на прерывание по спаду
+	sei(); //Глобальное разрешение прерываний
+	
+	while(1)
+	{
+		if(switch_state == 0)
+		{
+			Bin2Dec(cnt);
+			if(cnt < 9999)
+			{
+				cnt++;
+			}
+			else
+			{
+				cnt = 0;
+			}
+			_delay_ms(100);
+		}
+	}
+}
+
+
+//--------------------------------------------
+ISR(TIMER0_COMPA_vect)
+{
+	send_data(bcd_buffer[3], 0);
+	send_data(bcd_buffer[2], 1);
+	send_data(bcd_buffer[1], 2);
+	send_data(bcd_buffer[0], 3);
+}
+
+ISR(INT0_vect)
+{
+	if(switch_state == 0)
+	{
+		switch_state = 1;
+	}
+	else
+	{
+		switch_state = 0;
+		cnt = 0;
+	}
+}
+
+
+void InitPorts(void)
+{
+	DDRB = 0xFF;
+	DDRC = (1 << PC0 | 1 << PC1 | 1 << PC2 | 1 << PC3);
+	PORTC = 0x0F;
+	DDRD = (0 << PD2);
+	PORTD |= (1 << PD2);
+}
+void send_data(uint8_t data, uint8_t ind)
+{
+	PORTC = 0x0F & ~(1 << ind);
+	PORTB = segments[data];
+	_delay_ms(5);
+	PORTB = 0;
+	PORTC = 0x0F;
+}
+void InitTimer0(void)
+{
+	TCCR0A = (1 << WGM01); //режим CTC -Clear Timer on
+	//Compare
+	TCCR0B = (1 << CS02 | 1 << CS00);//prescaler = sys_clk/1024
+	TCNT0 = 0x00; //начальное значение счетчика
+	OCR0A = 16; //порог срабатывания
+	TIMSK0 |= (1<<OCIE0A); //включение прерывания при достижении порога А
+}
+void Bin2Dec(uint16_t data)
+{
+	bcd_buffer[3] = (uint8_t)(data / 1000);
+	data = data - bcd_buffer[3] * 1000;
+	bcd_buffer[2] = (uint8_t)(data / 100);
+	data = data - bcd_buffer[2] * 100;
+	bcd_buffer[1] = (uint8_t)(data / 10);
+	data = data - bcd_buffer[1] * 10;
+	bcd_buffer[0] = (uint8_t) data;
+}
